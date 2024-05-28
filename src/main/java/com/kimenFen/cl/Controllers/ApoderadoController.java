14917a71c5b45e443ddc4481a33190938fd23125
rizzo <bastianalonso92@gmail.com>
2024-05-28 11:07:16 -0400

    Version v0.3 -Agregar Anotacion(Admin-profesor) -Ver Anotacion(Admin-profesor-Apoderado) -Ver alumnos(Apoderado) -Correccion de error al momento de lista alumnos en menu admin tiraba conexion de error

---
+++ src/main/java/com/kimenFen/cl/Controllers/ApoderadoController.java
@@ -0,0 +1,38 @@
+package com.kimenFen.cl.Controllers;
+
+import com.kimenFen.cl.Model.Alumno;
+import com.kimenFen.cl.Repository.AlumnoRepository;
+import com.kimenFen.cl.Service.AlumnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class ApoderadoController{
+
+    @Autowired
+    private AlumnoRepository alumnoRepository;
+    @Autowired
+    private AlumnoService alumnoService;
+
+
+    @GetMapping("/apoderado/alumnos")
+    public String listarAlumnos(Model model) {
+        model.addAttribute("alumnos", alumnoRepository.findAll());
+        return "lista-alumnos-apoderado";
+    }
+
+    @GetMapping("/apoderado/ver-anotaciones/{id}")
+    public String verAnotaciones(@PathVariable("id") Long id, Model model) {
+        Alumno alumno = alumnoService.obtenerAlumnoPorId(id);
+        if (alumno != null) {
+            model.addAttribute("alumno", alumno);
+            model.addAttribute("anotaciones", alumno.getAnotaciones());
+        } else {
+            return "redirect:/apoderado/alumnos";
+        }
+        return "ver-anotaciones-apoderado";
+    }
+}
